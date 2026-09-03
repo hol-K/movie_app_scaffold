a9188e19f316d9717b6886097d44b082582fe3f4
Cedric Hnt <cedrichonoratoloukou@gmail.com>
2026-09-03 20:17:23 +0100

    Fix iOS build: Replace deprecated FlutterSceneDelegate

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -1,6 +1,17 @@
 import Flutter
 import UIKit
 
-class SceneDelegate: FlutterSceneDelegate {
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
 
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(windowScene: windowScene)
+    window?.rootViewController = FlutterViewController()
+    window?.makeKeyAndVisible()
+  }
 }
